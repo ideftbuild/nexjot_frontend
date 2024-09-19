743b19b0d6d0f7b083e7f161da8733ca0f02bb91
Maimunah <auspicious@RFS-002>
2024-09-19 15:17:10 +0100

    Implements the complete Landing Page with Header, Banner, Features, and Footer components; changed the styles of the dashboard

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -1,11 +1,15 @@
 import '../styles/header.css'
+import { Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 export const DashboardHeader = () => {
     return (
         <header>
-            <button><FaBars className={"fa-bars"}/></button>
-            <h1>NexJot</h1>
-        </header>
+            {/* <button><FaBars className={"fa-bars"}/></button> */}
+            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>
+              {/* Link to the homepage so when the NexJot is clicked it takes the user back to the homepage */}
+        <h1>NexJot</h1>
+      </Link>        
+      </header>
     )
 }
