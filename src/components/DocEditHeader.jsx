2084b95d413c4fe7074320614fb28ade803ad706
akanSWE <a.akanSWE@gamil.com>
2024-09-24 04:51:11 +0100

    feat: Fetch and render document in the edit page

--- src/components/DocEditHeader.jsx
+++ src/components/DocEditHeader.jsx
@@ -1,6 +1,5 @@
 import '../styles/header.css'
 import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa';
 import {handleChangeOnTitle} from "../listeners/document-listeners.js";
 import React from "react";
 
@@ -14,14 +13,13 @@ import React from "react";
 export const DocEditHeader = ({ title, setTitle } ) => {
     return (
         <header>
-            {/* <button><FaBars className={"fa-bars"}/></button> */}
-            <input maxLength={60} id={'title'} value={title}
+            <input maxLength={50} id={'title'} value={title}
                    onChange={handleChangeOnTitle(setTitle)}>
             </input>
-            <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <Link to='/dashboard' style={{ textDecoration: 'none', color: 'inherit' }}>
               {/* Link to the dashboard so when the logo is clicked it takes the user to the dashboard */}
-        <h1>NexJot</h1>
-      </Link>              
+                <h1>NexJot</h1>
+            </Link>
       </header>
     )
 }
